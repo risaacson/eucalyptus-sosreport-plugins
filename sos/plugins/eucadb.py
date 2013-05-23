779968f04ea829c72b34a97f305b3028f768ac46
Richard C Isaacson <richard@eucalyptus.com>
2013-05-23 09:33:22 -0500

    Minor cleanup.

--- sos/plugins/eucadb.py
+++ sos/plugins/eucadb.py
@@ -1,4 +1,4 @@
-## Copyright (C) 2012-2013 Eucalyptus Systems, Inc., Richard Isaacson <richard@eucalyptus.com>
+## Copyright (C) 2013 Eucalyptus Systems, Inc., Richard Isaacson <richard@eucalyptus.com>
 
 ### This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
