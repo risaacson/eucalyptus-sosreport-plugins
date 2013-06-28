818bb3fdb664bf42d39ff32dc9cb34a8d2b65922
Richard C Isaacson <richard@eucalyptus.com>
2013-06-28 15:39:26 -0500

    Additional plugins.

---
+++ sos/plugins/eucaconsole.py
@@ -0,0 +1,26 @@
+## Copyright (C) 2013 Eucalyptus Systems, Inc., Richard Isaacson <richard@eucalyptus.com>
+
+### This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from sos.plugins import Plugin, RedHatPlugin
+import os
+
+class eucaconsole(Plugin, RedHatPlugin):
+    """Eucalyptus Cloud - Console"""
+
+    packages = ('eucalyptus-console',)
+
+    def setup(self):
+        self.add_copy_specs(["/etc/eucalyptus-console"})
